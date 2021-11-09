7af927235b3ae9ec52cb7115b31ea9f54bfe3e06
davidchassatte <david.chassatte@gmail.com>
2021-11-09 14:35:37 +0100

    second commit

--- main.py
+++ main.py
@@ -33,8 +33,7 @@ def scrape_book_information():
         price_excluding_tax = product_information[2].string
         price_including_tax = product_information[3].string
         number_available = product_information[5].string
-        review_rating = product_information[6].string
-    
+
         product_description_bs = soup.find_all("p")
         product_description = product_description_bs[3].string
     
@@ -45,14 +44,32 @@ def scrape_book_information():
         images = image_bs[0]
         image_2 = images.attrs['src']
         image = 'https://books.toscrape.com/' + image_2.replace('../..', '')
+
+        # récupération du paragraphe star-rating
+        review_rating_bs = soup.find("p", class_='star-rating')
+        print('paragraphe star-rating récuperé:', review_rating_bs)
+
+        # récupération de la liste des classes du paragraphe
+        review_rating_classes = review_rating_bs['class']
+        print('liste des classes du paragraphe récupérées:', review_rating_classes)
+
+        # récupération de la position de star-rating dans le tableau review_rating_class
+        star_rating_index = review_rating_classes.index('star-rating')
+        print('position de star_rating récupéré:', star_rating_index)
+
+        # enlèvement de l'index star_rating_index
+        review_rating_classes.pop(star_rating_index)
+        print('afficher ce qui reste dans review_rating_classes:', review_rating_classes)
+
+        # récupération de ce qui reste dans review_rating_classes
+        review_rating = review_rating_classes[0] + ' star(s)'
+        print('ce qui est resté dans review_rating_classes:', review_rating)
         csv.write(
             title + ',' + upc + ',' + price_including_tax + ',' + price_excluding_tax + ',' + number_available + ',' +
             image + ',' + category + ',' + review_rating + ',' + product_description.replace(',', ''))    
 '''
 
 '''
-
-
 def scrape_one_category():
 
     # Scrapper toutes les urls de livres appartenant à une catégorie
@@ -79,7 +96,6 @@ def scrape_one_category():
                 link = a['href']
                 book_urls.append('https://books.toscrape.com/catalogue' + link.replace('../../..', ''))
             time.sleep(3)
-    print(book_urls)
 
     with open('scraping_one_category.csv', 'w', encoding="utf-8-sig") as file:
         file.write(
@@ -98,7 +114,6 @@ def scrape_one_category():
             price_excluding_tax = product_information[2].string.replace('Â', '')
             price_including_tax = product_information[3].string.replace('Â', '')
             number_available = product_information[5].string
-            review_rating = product_information[6].string
 
             product_description_bs = soup_2.find_all("p")
             product_description = product_description_bs[3].string.replace("\"", "")
@@ -110,6 +125,12 @@ def scrape_one_category():
             images = image_bs[0]
             image_2 = images.attrs['src']
             image = 'https://books.toscrape.com/' + image_2.replace('../..', '')
+
+            review_rating_bs = soup_2.find("p", class_='star-rating')
+            review_rating_classes = review_rating_bs['class']
+            star_rating_index = review_rating_classes.index('star-rating')
+            review_rating_classes.pop(star_rating_index)
+            review_rating = review_rating_classes[0] + ' star(s)'
             file.write(
                 title + ', ' + upc + ', ' + price_including_tax + ', ' + price_excluding_tax + ', ' +
                 number_available + ', ' + image + ', ' + category + ', ' +
@@ -117,75 +138,79 @@ def scrape_one_category():
 '''
 
 
-# Scrapper toutes les catégories
-all_category_urls = []
-url = 'https://books.toscrape.com/'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html.parser')
-category_url = soup.find('ul', class_='nav nav-list').findAll('a')
-for urls in category_url:
-    href = urls['href'].replace('index.html', '')
-    href_bs = url + href
-    all_category_urls.append(href_bs)
-del all_category_urls[0]
+def scrape_everything():
 
-# Ajouter les pages additionnelles des catégories
-full_category_urls = []
-for all_category_urls_1 in all_category_urls:
-    page_category = requests.get(all_category_urls_1)
-    soup_category = BeautifulSoup(page_category.text, "html.parser")
-    number_of_books_on_page = soup_category.find('form', class_="form-horizontal").find('strong').text
-    if int(number_of_books_on_page) > 20:
-        number_of_pages = ceil(int(number_of_books_on_page) / 20)
-        for i in range(1, number_of_pages + 1):
-            url_page = all_category_urls_1 + 'page-' + str(i) + '.html'
-            full_category_urls.append(url_page)
-    elif not int(number_of_books_on_page) > 20:
-        full_category_urls.append(all_category_urls_1)
+    # Scrapper toutes les catégories
+    all_category_urls = []
+    url = 'https://books.toscrape.com/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    category_url = soup.find('ul', class_='nav nav-list').findAll('a')
+    for urls in category_url:
+        href = urls['href'].replace('index.html', '')
+        href_bs = url + href
+        all_category_urls.append(href_bs)
+    del all_category_urls[0]
 
-# Scrapper les urls des livres
-all_books_urls = []
-for urls in full_category_urls:
-    page_book = requests.get(urls)
-    soup_book = BeautifulSoup(page_book.text, 'html.parser')
-    books = soup_book.findAll('div', class_="image_container")
-    for product in books:
-        a = product.find('a')
-        url_book = a['href'].replace('../../..', '')
-        url_book_bs = 'http://books.toscrape.com/catalogue' + url_book
-        all_books_urls.append(url_book_bs)
+    # Ajouter les pages additionnelles des catégories
+    full_category_urls = []
+    for all_category_urls_1 in all_category_urls:
+        page_category = requests.get(all_category_urls_1)
+        soup_category = BeautifulSoup(page_category.text, "html.parser")
+        number_of_books_on_page = soup_category.find('form', class_="form-horizontal").find('strong').text
+        if int(number_of_books_on_page) > 20:
+            number_of_pages = ceil(int(number_of_books_on_page) / 20)
+            for i in range(1, number_of_pages + 1):
+                url_page = all_category_urls_1 + 'page-' + str(i) + '.html'
+                full_category_urls.append(url_page)
+        elif not int(number_of_books_on_page) > 20:
+            full_category_urls.append(all_category_urls_1)
 
-with open('scraping_all_categories.csv', 'w', encoding="utf-8-sig") as file:
-    file.write(
-        "title,upc,price_including_tax,price_excluding_tax,number_available,"
-        "image,category,review_rating,product_description" + '\n')
-    for book in all_books_urls:
-        url_book = book.strip()
-        response = requests.get(url_book)
-        soup_2 = BeautifulSoup(response.text, 'html.parser')
+    # Scrapper les urls des livres
+    all_books_urls = []
+    for urls in full_category_urls:
+        page_book = requests.get(urls)
+        soup_book = BeautifulSoup(page_book.text, 'html.parser')
+        books = soup_book.findAll('div', class_="image_container")
+        for product in books:
+            a = product.find('a')
+            url_book = a['href'].replace('../../..', '')
+            url_book_bs = 'http://books.toscrape.com/catalogue' + url_book
+            all_books_urls.append(url_book_bs)
 
-        title_bs = soup_2.find_all("h1")
-        title = title_bs[0].string
+    with open('scraping_all_categories.csv', 'w', encoding='utf-8-sig') as file:
+        file.write("title,upc,price_excluding_tax,price_including_tax,number_available,"
+                   "image,category,review_rating,product_description" + '\n')
+        for book in all_books_urls:
+            url_book2 = book.strip()
+            response = requests.get(url_book2)
+            soup_2 = BeautifulSoup(response.text, 'html.parser')
 
-        product_information = soup_2.find_all("td")
-        upc = product_information[0].string
-        price_excluding_tax = product_information[2].string
-        price_including_tax = product_information[3].string
-        number_available = product_information[5].string
-        review_rating = product_information[6].string
+            title_bs = soup_2.find_all("h1")
+            title = title_bs[0].string.replace(",", "").replace("\"", "")
 
-        product_description_bs = soup_2.find_all("p")
-        product_description = product_description_bs[3].string
+            product_information = soup_2.find_all("td")
+            upc = product_information[0].string
+            price_excluding_tax = product_information[2].string.replace('Â', '')
+            price_including_tax = product_information[3].string.replace('Â', '')
+            number_available = product_information[5].string
 
-        category_bs = soup_2.find_all("a")
-        category = category_bs[3].string
+            product_description_bs = soup_2.find_all("p")
+            product_description = product_description_bs[3].string
 
-        image_bs = soup_2.find_all("img")
-        images = image_bs[0]
-        image_2 = images.attrs['src']
-        image = 'https://books.toscrape.com/' + image_2.replace('../..', '')
-        file.write(
-            title + ',' + upc + ',' + price_including_tax + ',' + price_excluding_tax + ',' +
-            number_available + ',' + image + ',' + category + ',' +
-            review_rating + ',' + product_description.replace(',', '') + '\n')
+            category_bs = soup_2.find_all("a")
+            category = category_bs[3].string
 
+            image_bs = soup_2.find_all("img")
+            images = image_bs[0]
+            image_2 = images.attrs['src']
+            image = 'https://books.toscrape.com/' + image_2.replace('../..', '')
+
+            review_rating_bs = soup_2.find("p", class_='star-rating')
+            review_rating_classes = review_rating_bs['class']
+            star_rating_index = review_rating_classes.index('star-rating')
+            review_rating_classes.pop(star_rating_index)
+            review_rating = review_rating_classes[0] + ' star(s)'
+            file.write(str(title) + ',' + str(upc) + ',' + str(price_excluding_tax) + ',' + str(price_including_tax) + ',' +
+                       str(number_available) + ',' + str(image) + ',' + str(category) + ',' + str(review_rating) + ',' +
+                       str(product_description).replace(",", "").replace("\"", "") + '\n')
